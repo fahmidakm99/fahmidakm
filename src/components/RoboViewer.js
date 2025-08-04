bab9e34de869f4d7ce47b364939fc8b06e37bf70
fahmida <fanihahaanifa@gmail.com>
2025-08-04 12:55:12 +0400

    modify for images

--- src/components/RoboViewer.js
+++ src/components/RoboViewer.js
@@ -4,7 +4,7 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 import { useRef } from "react";
 
 const Model = () => {
-  const gltf = useGLTF(`${process.env.PUBLIC_URL}/assets/cute.glb`);
+  const gltf = useGLTF(`${process.env.PUBLIC_URL}/assets/cute-optimized.glb`);
 
   const modelRef = useRef();
 
